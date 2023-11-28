c4b48a267d43cd6c512f39831f16b62d4f814319
Mateo <mateo.nicoloff25@gmail.com>
2023-11-27 21:54:32 -0300

    version 1.0

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,22 @@
- import "./nav.css"
+import "./nav.css";
 
- export default function Nav(){
-    return(   
-            <nav className="navegacion">
-            <img src="./icons/logo.svg" alt="Logo" className="navegacion__logo"/>                
-            <div className="navegacion__links">
-                    <a href="#features" className="links__item">Features</a>
-                    <a href="#stayProductive" className="links__item">Team</a>
-                    <a href="#earlyAccess" className="links__item">Sign in</a>
-                </div>
-            </nav>   
-    )
- }
+export default function Nav() {
+  return (
+    <div className="contenedor__nav">
+      <nav className="navegacion">
+        <img src="./icons/logo.svg" alt="Logo" className="navegacion__logo" />
+        <div className="navegacion__links">
+          <a href="#features" className="links__item">
+            Features
+          </a>
+          <a href="#stayProductive" className="links__item">
+            Team
+          </a>
+          <a href="#earlyAccess" className="links__item">
+            Sign in
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
